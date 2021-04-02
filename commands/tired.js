4ae9deec6c5aad169d85a2077e8e8c359c129327
Shiina631 <42161993+Shiina631@users.noreply.github.com>
2021-04-02 11:10:05 +0800

    asd

---
+++ commands/tired.js
@@ -0,0 +1,9 @@
+module.exports ={
+    name: 'tired',
+    description: "this is a tired command!",
+    execute(message, args){
+        if (!args.length) {
+            message.channel.send('https://cdn.discordapp.com/attachments/807501054483759124/824597394531418112/dJXGcpgaQPtBl-po3Ko2XVWFLGE9h1vpXJQ13TLaJncHllYuLaY6onllFoUNEeBN6HNr-7mP8bwg2XNtO8TkE0r86ml8_mdhNoHp.png');        
+        } 
+    }
+}
